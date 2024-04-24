a8f07f3a0f6e6dfc283aee5ba38553ef2c59055c
AlcivanLucas <alcivan14lucas@gmail.com>
2024-04-24 13:24:35 -0300

    feat: criar ratreamento de GenreID na page home,

--- home/assets/script/getIdsPreferences.js
+++ home/assets/script/getIdsPreferences.js
@@ -12,18 +12,39 @@ function getGenreIdsFromUrl() {
     // Separar os IDs dos gêneros usando espaço como separador
     const genreIds = idsParam ? idsParam.split(' ') : [];
 
+    genreIds.pop(); // Remove o último elemento do array, 
+    // Heitor depois remove 
+
     return genreIds;
 }
 
+const map = {
+    28: 'Ação',
+    12: 'Aventura',
+    16: 'Animação',
+    35: 'Comédia',
+    80: 'Crime',
+    99: 'Documentário',
+    18: 'Drama',
+    10751: 'Família',
+    14: 'Fantasia',
+    36: 'História',
+    27: 'Terror',
+    10402: 'Música',
+    9648: 'Mistério',
+    10749: 'Romance',
+    878: 'Ficção Científica',
+    10770: 'Cinema TV',
+    53: 'Thriller',
+    10752: 'Guerra',
+    37: 'Faroeste'
+};
 
 function fetchGenre() {
     const genreIds = getGenreIdsFromUrl();
     const categorySection = document.querySelector('.preference-categories');
 
     genreIds.forEach(genreId => {
-        // Aqui você pode buscar os detalhes do gênero usando a API do TMDB, se necessário
-        // Por exemplo, para obter o nome do gênero, você pode fazer uma chamada separada
-        // ou usar um mapeamento de IDs para nomes de gêneros, se disponível.
 
         // Cria a seção de categoria para o gênero específico
         const category = createGenreSection(genreId); // Modifique esta função para aceitar um ID de gênero
@@ -34,16 +55,11 @@ function fetchGenre() {
     });
 }
 
-// Função hipotética para buscar os detalhes dos gêneros
-function fetchGenreDetails(genreIds) {
-    // Implemente a lógica para buscar os detalhes dos gêneros usando a API do TMDB
-    // Retorna uma promessa que resolve para um array de objetos de gênero
-}
-
 
 function createGenreSection(genreId) {
     const category = document.createElement('div');
     category.className = 'category-grade';
+    console.log(genreId); // Exibe o ID do gênero na console
 
     // Cria o elemento div para conter a seção do gênero
     const genreSection = document.createElement('div');
@@ -51,7 +67,8 @@ function createGenreSection(genreId) {
     // Aqui você pode adicionar o nome do gênero, se necessário
     // Por exemplo, se você tiver um mapeamento de IDs para nomes de gêneros, você pode usá-lo
     const genreName = document.createElement('h2');
-    genreName.textContent = 'Nome do Gênero'; // Substitua 'Nome do Gênero' pelo nome real do gênero
+    genreName.textContent = map[genreId] || 'Gênero Desconhecido'; 
+    
     genreSection.appendChild(genreName);
 
     const movieRow = document.createElement('div');
@@ -69,3 +86,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const genreIds = getGenreIdsFromUrl();
     console.log("esses são os ids passados pela URL:",genreIds); // Exibe os IDs dos gêneros na console
 });
+
+
